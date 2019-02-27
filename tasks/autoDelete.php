620c2692c81507fc8c07a91396bc1c6f5c32025f
OnDebian <ondebian@protonmail.com>
2019-02-27 18:19:53 +0100

    Add copy/paste images from clipboard

--- tasks/autoDelete.php
+++ tasks/autoDelete.php
@@ -4,7 +4,7 @@ require __DIR__."/../utils/Loader.php";
 
 
 $clear = new YuuClear();
-$clear->upload_folder = Web::uploadFolder;
+$clear->upload_folder = __DIR__."/../uploads/";
 $clear->setDatabase(
 	Web::database["hostname"],
 	Web::database["port"],
